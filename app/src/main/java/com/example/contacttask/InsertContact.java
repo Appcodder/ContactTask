10547d5f0b6bc64ce94eeba4d9daa106f19e96ae
Woxxin 4 <apppcodder@gmail.com>
2024-03-29 12:10:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/contacttask/InsertContact.java
@@ -0,0 +1,14 @@
+package com.example.contacttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_contact);
+    }
+}
